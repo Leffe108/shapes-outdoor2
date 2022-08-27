fa2c88352cde07ddd49e9a0983ab0ece6ff08b8d
Leif Linse <leif.linse@gmail.com>
2022-08-27 22:59:50 +0200

    Fix: make start screen support smaller screens better

--- lib/screens/start/start_screen.dart
+++ lib/screens/start/start_screen.dart
@@ -10,90 +10,128 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context);
+    const margin = 50.0;
+    const iconMargin = 10.0;
+
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.all(50.0),
+        padding: EdgeInsets.fromLTRB(iconMargin, margin, iconMargin,
+            mq.size.height > 700 ? margin : iconMargin),
         color: Theme.of(context).colorScheme.background,
         child: SafeArea(
-          child: Column(children: [
-            Text(
-              'Shapes Outdoor',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            Expanded(child: Container()),
-            const SizedBox(height: 30),
-            GestureDetector(
-              onTap: () {
-                Routemaster.of(context).push('/new-game');
-              },
-              child: AspectRatio(
-                aspectRatio: 1 / 1,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.black,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Theme.of(context).brightness == Brightness.light
-                            ? Colors.black.withOpacity(0.10)
-                            : Colors.black.withOpacity(0.15),
-                        offset: const Offset(2, 2),
-                        blurRadius: 5,
-                        spreadRadius: 5,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                      margin - iconMargin, 0, margin - iconMargin, 0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Shapes Outdoor',
+                        style: Theme.of(context).textTheme.headlineSmall,
                       ),
+                      Expanded(child: Container()),
+                      const SizedBox(height: 30),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          maxWidth: mq.size.height < 700
+                              ? (mq.size.width - margin * 2) * 0.8
+                              : double.infinity,
+                        ),
+                        child: GestureDetector(
+                          onTap: () {
+                            Routemaster.of(context).push('/new-game');
+                          },
+                          child: AspectRatio(
+                            aspectRatio: 1 / 1,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.black,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Theme.of(context).brightness ==
+                                            Brightness.light
+                                        ? Colors.black.withOpacity(0.10)
+                                        : Colors.black.withOpacity(0.15),
+                                    offset: const Offset(2, 2),
+                                    blurRadius: 5,
+                                    spreadRadius: 5,
+                                  ),
+                                ],
+                              ),
+                              clipBehavior: Clip.antiAlias,
+                              child: FadeIn(
+                                duration: const Duration(milliseconds: 700),
+                                child: Image.asset('assets/start-image.jpg'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                          'Collect shapes in your neigbourhood to complete this game.'),
+                      const SizedBox(height: 20),
+                      Expanded(child: Container()),
+                      StadiumButton(
+                        text: const Text('Start game'),
+                        onPressed: () {
+                          Routemaster.of(context).push('/new-game');
+                        },
+                      ),
+                      const SizedBox(height: 20),
                     ],
-                  ),
-                  clipBehavior: Clip.antiAlias,
-                  child: FadeIn(
-                    duration: const Duration(milliseconds: 700),
-                    child: Image.asset('assets/start-image.jpg'),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-                'Collect shapes in your neigbourhood to complete this game.'),
-            const SizedBox(height: 20),
-            Expanded(child: Container()),
-            StadiumButton(
-              text: const Text('Start game'),
-              onPressed: () {
-                Routemaster.of(context).push('/new-game');
-              },
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.settings),
-                  onPressed: () {
-                    showSettingsDialog(context);
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.question_mark),
-                  onPressed: () {
-                    showAboutDialog(
-                        applicationName: 'Shapes Outdoor 2',
-                        context: context,
-                        children: [
-                          StadiumButton(
-                            text: const Text('Privacy Policy'),
-                            onPressed: () {
-                              final uri = Uri.parse(
-                                  'https://junctioneer.net/shapes-outdoor2/privacy-policy/');
-                              launchUrl(uri, mode: LaunchMode.inAppWebView);
-                            },
-                          ),
-                        ]);
-                  },
-                ),
-              ],
-            ),
-          ]),
+              buildIconButtons(context, margin - iconMargin),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget buildIconButtons(BuildContext context, double height) {
+    return SizedBox(
+      height: height,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              showSettingsDialog(context);
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.question_mark),
+            onPressed: () {
+              showAboutDialog(
+                  applicationName: 'Shapes Outdoor 2',
+                  context: context,
+                  children: [
+                    StadiumButton(
+                      text: const Text('Privacy Policy'),
+                      onPressed: () {
+                        final uri = Uri.parse(
+                            'https://junctioneer.net/shapes-outdoor2/privacy-policy/');
+                        launchUrl(uri, mode: LaunchMode.inAppWebView);
+                      },
+                    ),
+                  ]);
+            },
+          ),
+        ],
       ),
     );
   }
