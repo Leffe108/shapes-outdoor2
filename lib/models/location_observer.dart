dfc87867b2c4a4efc25767da7656956cd817add5
Leif Linse <leif.linse@gmail.com>
2023-01-03 20:06:18 +0100

    Remove: unused location_observer.dart file

--- lib/models/location_observer.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-import 'package:location/location.dart';
-
-class LocationManager {
-  bool? _background;
-  late Stream<LocationData> _stream;
-  late StreamSubscription<LocationData> _streamSubscription;
-}
